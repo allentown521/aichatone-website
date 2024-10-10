1dbacc213621e9925f54f8ad0637e1828cd4d69b
allentown <tangxiangpi@gmail.com>
2024-10-10 22:24:26 +0800

    use img

--- middleware.ts
+++ middleware.ts
@@ -61,6 +61,5 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   // Matcher ignoring `/_next/` and `/api/`
-  matcher: [// some matcher that includes `/images`
-    ],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
