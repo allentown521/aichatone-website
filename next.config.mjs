fdd86cffe129454b67697e6ed65a89b3b91967cd
allentown <tangxiangpi@gmail.com>
2024-10-11 10:37:01 +0800

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"]
-    });
-    return config;
-  },
-  images: {
-    dangerouslyAllowSVG: true,
-    contentDispositionType: 'attachment',
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    domains: ['*'], // Add your domain or '*' to allow all domains
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
