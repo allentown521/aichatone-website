927d39744cdaef47927c65a9588b09560d1d0f00
allentown <tangxiangpi@gmail.com>
2025-02-14 10:44:01 +0800

    feat(app): 添加 Google Ads 跟踪
    - 添加 Google Ads 跟踪脚本

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -22,23 +22,25 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   robots: "max-image-preview:large",
-  keywords: "AIChatOne,ai chatone, aichatone web, Chrome extension, aichatone windows, aichatone mac, ChatGPT, Gemini, Claude, Bing Copilot, AI conversations, Typingmind, Chathub, Chatbox, chatbox desktop, chatbox windows, chat hub, chat box",
+  keywords:
+    "AIChatOne,ai chatone, aichatone web, Chrome extension, aichatone windows, aichatone mac, ChatGPT, Gemini, Claude, Bing Copilot, AI conversations, Typingmind, Chathub, Chatbox, chatbox desktop, chatbox windows, chat hub, chat box",
   // Add the canonical URL here
   alternates: {
-    canonical: 'https://aichatone.com/',
+    canonical: "https://aichatone.com/",
   },
   openGraph: {
-    type: 'website',
-    url: 'https://aichatone.com/',
-    title: 'AIChatOne : ALL-In-One ChatGPT Copilot & chathub & chatbox',
-    description: 'Free Chrome extension for interactive chats with ChatGPT, Gemini, Claude, and Bing Copilot. Enjoy seamless AI conversations and switch between Typingmind, Chathub, and Chatbox to enhance entertainment, information sourcing, or productivity',
-    siteName: 'AIChatOne',
+    type: "website",
+    url: "https://aichatone.com/",
+    title: "AIChatOne : ALL-In-One ChatGPT Copilot & chathub & chatbox",
+    description:
+      "Free Chrome extension for interactive chats with ChatGPT, Gemini, Claude, and Bing Copilot. Enjoy seamless AI conversations and switch between Typingmind, Chathub, and Chatbox to enhance entertainment, information sourcing, or productivity",
+    siteName: "AIChatOne",
     images: [
       {
-        url: 'https://framerusercontent.com/images/5iMLpc3RvbViTmHDN5lCQvEy2w.png', // 替换为你的实际 OG 图片 URL
+        url: "https://framerusercontent.com/images/5iMLpc3RvbViTmHDN5lCQvEy2w.png", // 替换为你的实际 OG 图片 URL
         width: 1200,
         height: 630,
-        alt: 'AI Chat One Preview',
+        alt: "AI Chat One Preview",
       },
     ],
   },
@@ -55,25 +57,51 @@ export default async function RootLayout({
   const dictionary = await getDictionary(params.lang);
   return (
     <html lang={params.lang}>
-       <head>
+      <head>
         <title>{dictionary?.pageTitle}</title>
         <meta name="description" content={dictionary?.pageDescription} />
         <link rel="alternate" hrefLang="en" href={`https://aichatone.com/en`} />
-        <link rel="alternate" hrefLang="zh-CN" href={`https://aichatone.com/zh-CN`} />
-        <link rel="alternate" hrefLang="zh-TW" href={`https://aichatone.com/zh-TW`} />
+        <link
+          rel="alternate"
+          hrefLang="zh-CN"
+          href={`https://aichatone.com/zh-CN`}
+        />
+        <link
+          rel="alternate"
+          hrefLang="zh-TW"
+          href={`https://aichatone.com/zh-TW`}
+        />
         <link rel="alternate" hrefLang="ja" href={`https://aichatone.com/ja`} />
         <link rel="alternate" hrefLang="de" href={`https://aichatone.com/de`} />
         <link rel="alternate" hrefLang="fr" href={`https://aichatone.com/fr`} />
-        <link rel="alternate" hrefLang="x-default" href="https://aichatone.com" />
+        <link
+          rel="alternate"
+          hrefLang="x-default"
+          href="https://aichatone.com"
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
         <Script
-          src="https://umami.aichatone.com/script.js" data-website-id="f2919cc6-2394-4136-8ec7-19cd4b36b0f3"
+          src="https://umami.aichatone.com/script.js"
+          data-website-id="f2919cc6-2394-4136-8ec7-19cd4b36b0f3"
           strategy="afterInteractive"
         />
+        {/* AW-11065067122 是 Google ads不是Google Analytics的id */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=AW-11065067122`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-11065067122');
+          `}
+        </Script>
       </body>
     </html>
   );
